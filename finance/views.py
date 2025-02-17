6f2d348f14c7b786e5d9952cb656a6f17f8f2ad4
Exmar <polevdenis74@gmail.com>
2025-02-18 01:05:51 +0500

    Базовая система сайта со всеми шаблонами и системой регистрации

---
+++ finance/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login, logout, authenticate
+
+# Create your views here.
+
+def home(request):
+     return render(request, 'finance/home.html')
+
+def signup_user(request):
+    if request.method == 'GET':
+        return render(request, 'finance/signupuser.html', {'form':UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('current_finance')
+            except: IntegrityError
+            return render(request, 'finance/signupuser.html', {'form':UserCreationForm(), 'error':'Имя пользователя уже существует, пожалуйста задайте новое'})
+        else:
+            return render(request, 'finance/signupuser.html', {'form':UserCreationForm(), 'error':'Пароли не совпадают'})
+
+def login_user(request):
+    if request.method == 'GET':
+        return render(request, 'finance/signupuser.html', {'form':AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'finance/signupuser.html', {'form':AuthenticationForm(), 'error':' Username And Password Did not match'})
+        else:
+           login(request, user)
+           return redirect('current_finance')
+
+def logout_user(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
+
+def current_finance(request):
+   return render(request, 'finance/current_finance.html') 
+
+        
---
+++ myproject/urls.py
@@ -0,0 +1,33 @@
+"""
+URL configuration for myproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from  finance import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+		
+    #Auth
+	path('signup/', views.signup_user, name='signupuser'),
+	path('login/', views.login_user, name='loginuser'),
+	path('logout/', views.logout_user, name='logoutuser'),
+	
+    #Finance
+	path('', views.home, name='home'),	
+	path('current/', views.current_finance, name='current_finance'),
+]
+ 
