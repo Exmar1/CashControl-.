4a15503ca586f0633d9f1b15bf20d142c831e3b9
Exmar <polevdenis74@gmail.com>
2025-02-20 15:48:35 +0500

    Версия добавления транзакции с багами

--- finance/models.py
+++ finance/models.py
@@ -21,7 +21,7 @@ class Transaction(models.Model):
 	category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
 	amount = models.DecimalField(max_digits=10, decimal_places=2)
 	transaction_type = models.CharField(max_length=9, choices=TRANSACTION_TYPE)
-	date = models.DateTimeField(auto_now_add=True)
+	date = models.DateTimeField(null=True, blank=True)
 	description = models.TextField(blank=True)
 
 	def __str__(self):
